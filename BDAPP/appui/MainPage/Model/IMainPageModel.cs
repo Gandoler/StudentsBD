e3d5f3d23f60545d929067a97efe9d0c11a598b7
glkru <gl.krutoi@mail.ru>
2025-02-25 16:23:30 +0300

    заполнил интерфейс

--- BDAPP/appui/MainPage/Model/IMainPageModel.cs
+++ BDAPP/appui/MainPage/Model/IMainPageModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace BDAPP.appui.MainPage.Model
 {
     interface IMainPageModel
     {
+        public void AddMark(StudentFieldTemplates studentFieldTemplates);
+
+        public void UpdateMark(StudentFieldTemplates studentFieldTemplates);
+
+        public void DeleteMark(StudentFieldTemplates studentFieldTemplates);
+
+        public DataTable searchById(StudentFieldTemplates studentFieldTemplates);
+
+        public DataTable searchByNames(StudentFieldTemplates studentFieldTemplates);
+
+        public DataTable searchByFieldAndID(StudentFieldTemplates studentFieldTemplates);
+
+
+        public DataTable GetFullTable();
     }
 }
