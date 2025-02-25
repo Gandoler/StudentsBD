2469cc355facd42f6c90587b866ce57ad81e3e1c
glkru <gl.krutoi@mail.ru>
2025-02-25 14:41:44 +0300

    делаем меин пейдж

--- BDAPP/appui/MainPage/Model/MainPageModel.cs
+++ BDAPP/appui/MainPage/Model/MainPageModel.cs
@@ -1,30 +1,78 @@
-﻿using System;
+﻿using BDAPP.logic.DBTools.Managers.Connection;
+using BDAPP.logic.DBTools.Managers.Data;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+//CREATE OR REPLACE FUNCTION FIELDID_BY_Name(filedNameInput varchar(100))
+//RETURNS uuid
+//LANGUAGE plpgsql
+//AS $$
+//DECLARE 
+//    FIELDID uuid;
+//BEGIN
+//    SELECT field_id INTO FIELDID
+//    FROM fields
+//    WHERE fields.field_name = filedNameInput;
+
+//IF NOT FOUND THEN
+//        RETURN NULL;
+//END IF;
+
+//RETURN FIELDID;
+//END;
+//$$;
+
 
 namespace BDAPP.appui.MainPage.Model
 {
     class MainPageModel: IMainPageModel
     {
 
-        public void AddMark()
+        private readonly string _role;
+        private readonly IDataManager _dataManager;
+
+        public MainPageModel(IDataManager dataManager, string role)
+        {
+            _dataManager = dataManager;
+            _role = role;
+        }
+
+        public void AddMark(StudentFieldTemplates studentFieldTemplates)
+        {
+            if (_role != "admin")
+            {
+                throw new UnauthorizedAccessException("Недостаточно прав.");
+            }
+
+            string query = "" +
+                "" +
+                "" +
+                "INSERT INTO field_comprehensions (student_id, field, mark) VALUES (@studentId, @field, @mark)";
+
+            var parameters = new[]
+            {
+            new SqlParameter("@studentId", SqlDbType.Int) { Value = studentFieldTemplates.Student_id },
+            new SqlParameter("@field", SqlDbType.UniqueIdentifier) { Value = studentFieldTemplates.Field_Name },
+            new SqlParameter("@mark", SqlDbType.Int) { Value = studentFieldTemplates.Mark }
+        };
+
+            _dataManager.ExecuteQuery(query, parameters);
+        }
+
+        public void UpdateMark(StudentFieldTemplates studentFieldTemplates)
         {
 
         }
 
-        public void UpdateMark()
+        public void DeleteMark(StudentFieldTemplates studentFieldTemplates)
         {
 
         }
 
-        public void DeleteMark()
-        {
-
-        }
-
-        public void search()
+        public void search(StudentFieldTemplates studentFieldTemplates)
         {
 
         }
---
+++ BDAPP/logic/DBTools/Managers/CRUD/CrudManager.cs
@@ -0,0 +1,22 @@
+﻿using BDAPP.logic.DBTools.Managers.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDAPP.logic.DBTools.Managers.CRUD
+{
+    class CrudManager
+    {
+        private readonly IDataManager _dataManager;
+
+        public CrudManager(IDataManager dataManager)
+        {
+            _dataManager = dataManager;
+        }
+
+
+
+    }
+}
