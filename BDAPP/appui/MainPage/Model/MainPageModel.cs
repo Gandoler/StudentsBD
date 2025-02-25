b941ba97b4f324cab8a0b4489fcdce50cc1f9d22
glkru <gl.krutoi@mail.ru>
2025-02-25 16:06:54 +0300

    ввыв

--- BDAPP/appui/MainPage/Model/MainPageModel.cs
+++ BDAPP/appui/MainPage/Model/MainPageModel.cs
@@ -6,24 +6,7 @@ using System.Data;
 using System.Data.SqlClient;
 
 
-//CREATE OR REPLACE FUNCTION FIELDID_BY_Name(filedNameInput varchar(100))
-//RETURNS uuid
-//LANGUAGE plpgsql
-//AS $$
-//DECLARE 
-//    FIELDID uuid;
-//BEGIN
-//    SELECT field_id INTO FIELDID
-//    FROM fields
-//    WHERE fields.field_name = filedNameInput;
 
-//IF NOT FOUND THEN
-//        RETURN NULL;
-//END IF;
-
-//RETURN FIELDID;
-//END;
-//$$;
 
 
 namespace BDAPP.appui.MainPage.Model
